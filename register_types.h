de66e1333f1b11f11249457743ce68ba7f4f378f
darthLeviN <darthleviellis@gmail.com>
2023-04-03 14:36:06 +0300

    Revert "Godot 4 api update."
    This reverts commit a2408deb7691e869f16a588c1fff97c6596d6cf2.
    
    This commit was tested and seemed to be needed although it wasn't aligned with the documentation. on further tries it seems like it was the wrong thing to do.

--- register_types.h
+++ register_types.h
@@ -9,10 +9,7 @@
 #include "modules/register_module_types.h"
 
 
-// void initialize_auidio_module(ModuleInitializationLevel p_level);
-// void uninitialize_auidio_module(ModuleInitializationLevel p_level);
-
-void register_auidio_types();
-void unregister_auidio_types();
+void initialize_auidio_module(ModuleInitializationLevel p_level);
+void uninitialize_auidio_module(ModuleInitializationLevel p_level);
 
 #endif // EXAMPLE_REGISTER_TYPES_H
